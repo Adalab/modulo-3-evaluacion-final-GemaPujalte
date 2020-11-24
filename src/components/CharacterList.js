c433ecd6ba867e6aaa1996b333495617d0ed2ee0
GemaPujalte <gema.pujalte.iglesias@gmail.com>
2020-11-24 09:19:03 +0100

    paint elements

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,25 @@
-const CharacterList = () => {
-  return <div>Holaaaaa</div>;
+import React from "react";
+import CharacterCard from "./CharacterCard";
+
+const CharacterList = (props) => {
+  const charactersItems = props.characters.map((character) => {
+    console.log(character.image);
+
+    return (
+      <CharacterCard
+        key={character.id}
+        image={character.image}
+        name={character.name}
+        species={character.species}
+      />
+    );
+  });
+  return (
+    <div>
+      Holaaaaa
+      <section>{charactersItems}</section>
+    </div>
+  );
 };
 
 export default CharacterList;
